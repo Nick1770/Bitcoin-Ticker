a05591759867cdd619f7397f1379ae8c924f442f
Nick DiSisto <Nickolas1770@gmail.com>
2023-01-26 11:35:20 -0500

    Moved everything to one API call, removed some imports and droped a few coins

--- Ticker.py
+++ Ticker.py
@@ -4,10 +4,8 @@
 # See LICENSE.rst for details.
 # Additions by Nick DiSisto
 
-import re
-import time
 import requests
-import argparse
+from os import device_encoding
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
@@ -16,137 +14,40 @@ from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 
-
 # Create matrix device
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=24 , block_orientation=-90, rotate=2)
 print("Printing Bitcoin and Ethereum prices")
 
-# Fetch initial coin price
 
-# Bitcoin
-bdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
-bjson = bdata.json()
-print("Bitcoin $" + str (bjson['bitcoin']['usd']))
-
-# Ethereum
-edata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd")
-ejson = edata.json()
-print("Ethereum $" + str (ejson['ethereum']['usd']))
-
-# Bnb
-bnbdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd")
-bnbjson = bnbdata.json()
-print("BNB $" + str (bnbjson['binancecoin']['usd']))
-
-# Dogecoin
-ddata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd")
-djson = ddata.json()
-print("Dogecoin $" + str (djson['dogecoin']['usd']))
-
-# Uniswap
-udata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=uniswap&vs_currencies=usd")
-ujson = udata.json()
-print("Uniswap $" + str (ujson['uniswap']['usd']))
-
-# The Graph
-gdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=the-graph&vs_currencies=usd")
-gjson = gdata.json()
-print("The Graph $" + str (gjson['the-graph']['usd']))
-
-# NuCypher
-ndata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=nucypher&vs_currencies=usd")
-njson = ndata.json()
-print("NuCypher $" + str (njson['nucypher']['usd']))
-
-# Safemoon
-sdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=safemoon-2&vs_currencies=usd")
-sjson = sdata.json()
-print("SafeMoon $" + str (sjson['safemoon-2']['usd']))
-
-# WadzPay
-wdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=wadzpay-token&vs_currencies=usd")
-wjson = wdata.json()
-print("WadzPay $" + str (wjson['wadzpay-token']['usd']))
-
-# Crypto.com
-cdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=crypto-com-chain&vs_currencies=usd")
-cjson = cdata.json()
-print("Crypto.com Coin $" + str (cjson['crypto-com-chain']['usd']))
-
-# XRP
-xdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=ripple&vs_currencies=usd")
-xjson = xdata.json()
-print("XRP $" + str (xjson['ripple']['usd']))
-
-# SAFUU
-safdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=safuu&vs_currencies=usd")
-safjson = safdata.json()
-print("SAFUU $" + str (safjson['safuu']['usd']))
-
-
-# starts here
 while(1):
 
-	# Print message to LED
-	show_message(device, "Bitcoin $" + str (bjson['bitcoin']['usd']) + "          " + "Ethereum $" + str (ejson['ethereum']['usd']) + "          " + "BNB $" + str (bnbjson['binancecoin']['usd']) + "          " +  "Dogecoin $" + str (djson['dogecoin']['usd']) + "          " + "Uniswap $" + str (ujson['uniswap']['usd']) + "          " + "The Graph $" + str (gjson['the-graph']['usd']) + "          " + "NuCypher $" + str (njson['nucypher']['usd']) + "          " + "SafeMoon $" + str (sjson['safemoon-2']['usd']) + "          " + "WadzPay $" + str (wjson['wadzpay-token']['usd']) + "          " + "Crypto.com Coin $" + str (cjson['crypto-com-chain']['usd'])+ "          " + "XRP $" + str (xjson['ripple']['usd']) + "          " + "SAFUU $" + str (safjson['safuu']['usd']), fill="white", font=proportional(LCD_FONT),scroll_delay = 0.04)
-	
-	# Fetch half new data
-
-	# Bitcoin
-	bdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+	# Fetch coin prices
+	bdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Cdogecoin%2Cuniswap%2Cthe-graph%2Csafemoon-2%2Ccrypto-com-chain%2Csafuu&vs_currencies=usd")
 	bjson = bdata.json()
-	print("Bitcoin $" + str (bjson['bitcoin']['usd']))
-	# Bnb
-	bnbdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd")
-	bnbjson = bnbdata.json()
-	print("BNB $" + str (bnbjson['binancecoin']['usd']))
-	# Uniswap
-	udata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=uniswap&vs_currencies=usd")
-	ujson = udata.json()
-	print("Uniswap $" + str (ujson['uniswap']['usd']))
-	# NuCypher
-	ndata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=nucypher&vs_currencies=usd")
-	njson = ndata.json()
-	print("NuCypher $" + str (njson['nucypher']['usd']))
-	# Crypto.com
-	cdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=crypto-com-chain&vs_currencies=usd")
-	cjson = cdata.json()
-	print("Crypto.com Coin $" + str (cjson['crypto-com-chain']['usd']))
-	# SAFUU
-	safdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=safuu&vs_currencies=usd")
-	safjson = safdata.json()
-	print("SAFUU $" + str (safjson['safuu']['usd']))
 
-	# Fetch other half of new data
+	# Print coin prices to console
+	print("Bitcoin $" + str (bjson['bitcoin']['usd']))
+	print("Ethereum $" + str (bjson['ethereum']['usd']))
+	print("BNB $" + str (bjson['binancecoin']['usd']))
+	print("XRP $" + str (bjson['ripple']['usd']))
+	print("Dogecoin $" + str (bjson['dogecoin']['usd']))
+	print("Uniswap $" + str (bjson['uniswap']['usd']))
+	print("Crypto.com Coin $" + str (bjson['crypto-com-chain']['usd']))
+	print("The Graph $" + str (bjson['the-graph']['usd']))
+	print("SafeMoon $" + str (bjson['safemoon-2']['usd']))
+	print("SAFUU $" + str (bjson['safuu']['usd']))
+
 
 	# Print message to LED
-	show_message(device, "Bitcoin $" + str (bjson['bitcoin']['usd']) + "          " + "Ethereum $" + str (ejson['ethereum']['usd']) + "          " + "BNB $" + str (bnbjson['binancecoin']['usd']) + "          " +  "Dogecoin $" + str (djson['dogecoin']['usd']) + "          " + "Uniswap $" + str (ujson['uniswap']['usd']) + "          " + "The Graph $" + str (gjson['the-graph']['usd']) + "          " + "NuCypher $" + str (njson['nucypher']['usd']) + "          " + "SafeMoon $" + str (sjson['safemoon-2']['usd']) + "          " + "WadzPay $" + str (wjson['wadzpay-token']['usd']) + "          " + "Crypto.com Coin $" + str (cjson['crypto-com-chain']['usd'])+ "          " + "XRP $" + str (xjson['ripple']['usd']) + "          " + "SAFUU $" + str (safjson['safuu']['usd']), fill="white", font=proportional(LCD_FONT),scroll_delay = 0.04)
-	# Ethereum
-	edata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd")
-	ejson = edata.json()
-	print("Ethereum $" + str (ejson['ethereum']['usd']))
-	# Dogecoin
-	ddata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd")
-	djson = ddata.json()
-	print("Dogecoin $" + str (djson['dogecoin']['usd']))
-	# The Graph
-	gdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=the-graph&vs_currencies=usd")
-	gjson = gdata.json()
-	print("The Graph $" + str (gjson['the-graph']['usd']))
-	# Safemoon
-	sdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=safemoon-2&vs_currencies=usd")
-	sjson = sdata.json()
-	print("SafeMoon $" + str (sjson['safemoon-2']['usd']))
-	# WadzPay
-	wdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=wadzpay-token&vs_currencies=usd")
-	wjson = wdata.json()
-	print("WadzPay $" + str (wjson['wadzpay-token']['usd']))
-	# Crypto.com
-	cdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=crypto-com-chain&vs_currencies=usd")
-	cjson = cdata.json()
-	print("Crypto.com Coin $" + str (cjson['crypto-com-chain']['usd']))
-	# XRP
-	xdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=ripple&vs_currencies=usd")
-	xjson = xdata.json()
-	print("XRP $" + str (xjson['ripple']['usd']))
+	show_message(device, "Bitcoin $" + str (bjson['bitcoin']['usd']) + "          " +\
+	"Ethereum $" + str (bjson['ethereum']['usd']) + "          " +\
+	"BNB $" + str (bjson['binancecoin']['usd']) + "          " +\
+	"XRP $" + str (bjson['ripple']['usd']) + "          " +\
+	"Dogecoin $" + str (bjson['dogecoin']['usd']) + "          " +\
+	"Uniswap $" + str (bjson['uniswap']['usd']) + "          " +\
+	"Crypto.com Coin $" + str (bjson['crypto-com-chain']['usd']) + "          " +\
+	"The Graph $" + str (bjson['the-graph']['usd']) + "          " +\
+	"SafeMoon $" + str (bjson['safemoon-2']['usd']) + "          " +\
+	"SAFUU $" + str (bjson['safuu']['usd']) + "          " ,\
+	fill="white", font=proportional(LCD_FONT),scroll_delay = 0.04)
