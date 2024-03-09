8da1edcc4c4da7f61998e0748d1005f84406a103
Nickolas DiSisto <nick@bis-172-22-91-101.dhcp.muohio.edu>
2024-03-09 18:51:39 -0500

    Added a try and wait to account for too many API calls error.

--- Ticker.py
+++ Ticker.py
@@ -12,42 +12,45 @@ from luma.core.render import canvas
 from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
-
+import time
 
 # Create matrix device
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=24 , block_orientation=-90, rotate=2)
 print("Printing Bitcoin and Ethereum prices")
 
+while True:
+    try:
+        # Fetch coin prices
+        bdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Cdogecoin%2Cuniswap%2Cthe-graph%2Csafemoon-2%2Ccrypto-com-chain%2Csafuu&vs_currencies=usd")
+        bjson = bdata.json()
 
-while(1):
+        # Print coin prices to console
+        print("Bitcoin $" + str (bjson['bitcoin']['usd']))
+        print("Ethereum $" + str (bjson['ethereum']['usd']))
+        print("BNB $" + str (bjson['binancecoin']['usd']))
+        print("XRP $" + str (bjson['ripple']['usd']))
+        print("Dogecoin $" + str (bjson['dogecoin']['usd']))
+        print("Uniswap $" + str (bjson['uniswap']['usd']))
+        print("Crypto.com Coin $" + str (bjson['crypto-com-chain']['usd']))
+        print("The Graph $" + str (bjson['the-graph']['usd']))
+        print("SafeMoon $" + str (bjson['safemoon-2']['usd']))
+        print("SAFUU $" + str (bjson['safuu']['usd']))
 
-	# Fetch coin prices
-	bdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cbinancecoin%2Cripple%2Cdogecoin%2Cuniswap%2Cthe-graph%2Csafemoon-2%2Ccrypto-com-chain%2Csafuu&vs_currencies=usd")
-	bjson = bdata.json()
+        # Print message to LED
+        show_message(device, "Bitcoin $" + str (bjson['bitcoin']['usd']) + "          " +\
+            "Ethereum $" + str (bjson['ethereum']['usd']) + "          " +\
+            "BNB $" + str (bjson['binancecoin']['usd']) + "          " +\
+            "XRP $" + str (bjson['ripple']['usd']) + "          " +\
+            "Dogecoin $" + str (bjson['dogecoin']['usd']) + "          " +\
+            "Uniswap $" + str (bjson['uniswap']['usd']) + "          " +\
+            "Crypto.com Coin $" + str (bjson['crypto-com-chain']['usd']) + "          " +\
+            "The Graph $" + str (bjson['the-graph']['usd']) + "          " +\
+            "SafeMoon $" + str (bjson['safemoon-2']['usd']) + "          " +\
+            "SAFUU $" + str (bjson['safuu']['usd']) + "          " ,\
+            fill="white", font=proportional(LCD_FONT), scroll_delay=0.04)
 
-	# Print coin prices to console
-	print("Bitcoin $" + str (bjson['bitcoin']['usd']))
-	print("Ethereum $" + str (bjson['ethereum']['usd']))
-	print("BNB $" + str (bjson['binancecoin']['usd']))
-	print("XRP $" + str (bjson['ripple']['usd']))
-	print("Dogecoin $" + str (bjson['dogecoin']['usd']))
-	print("Uniswap $" + str (bjson['uniswap']['usd']))
-	print("Crypto.com Coin $" + str (bjson['crypto-com-chain']['usd']))
-	print("The Graph $" + str (bjson['the-graph']['usd']))
-	print("SafeMoon $" + str (bjson['safemoon-2']['usd']))
-	print("SAFUU $" + str (bjson['safuu']['usd']))
-
-
-	# Print message to LED
-	show_message(device, "Bitcoin $" + str (bjson['bitcoin']['usd']) + "          " +\
-	"Ethereum $" + str (bjson['ethereum']['usd']) + "          " +\
-	"BNB $" + str (bjson['binancecoin']['usd']) + "          " +\
-	"XRP $" + str (bjson['ripple']['usd']) + "          " +\
-	"Dogecoin $" + str (bjson['dogecoin']['usd']) + "          " +\
-	"Uniswap $" + str (bjson['uniswap']['usd']) + "          " +\
-	"Crypto.com Coin $" + str (bjson['crypto-com-chain']['usd']) + "          " +\
-	"The Graph $" + str (bjson['the-graph']['usd']) + "          " +\
-	"SafeMoon $" + str (bjson['safemoon-2']['usd']) + "          " +\
-	"SAFUU $" + str (bjson['safuu']['usd']) + "          " ,\
-	fill="white", font=proportional(LCD_FONT),scroll_delay = 0.04)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print("Waiting 60 seconds before trying again...")
+        time.sleep(60)
