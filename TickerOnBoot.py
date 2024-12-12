644de0b35b67f5e29852faa70f9ef3f12f19a2ed
Nickolas DiSisto <nick@bis-172-22-91-101.dhcp.muohio.edu>
2024-12-12 11:34:08 -0500

    Moved is_within_time_range to if/else statement within while True

--- TickerOnBoot.py
+++ TickerOnBoot.py
@@ -42,56 +42,60 @@ print("Printing Bitcoin and Ethereum prices")
 def is_within_time_range():
     # Get the current time
     current_time = datetime.now().time()
-    
+
     # Define the start and end times (9:00 AM to 10:00 PM)
     start_time = datetime.strptime("09:00:00", "%H:%M:%S").time()
     end_time = datetime.strptime("22:00:00", "%H:%M:%S").time()
-    
+
     # Check if the current time is within the defined range
     return start_time <= current_time <= end_time
 
-while is_within_time_range():
-    try:
-        # Fetch coin prices
-        bdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Csolana%2Csui%2Chedera-hashgraph%2Ctron%2Cthe-open-network%2Cbinancecoin%2Cripple%2Cdogecoin%2Cuniswap%2Cthe-graph%2Csafemoon-2%2Ccrypto-com-chain%2Csafuu&vs_currencies=usd")
-        bjson = bdata.json()
+while True:
+    if is_within_time_range():
+        try:
+            # Fetch coin prices
+            bdata = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Csolana%2Csui%2Chedera-hashgraph%2Ctron%2Cthe-open-network%2Cbinancecoin%2Cripple%2Cdogecoin%2Cuniswap%2Cthe-graph%2Csafemoon-2%2Ccrypto-com-chain%2Csafuu&vs_currencies=usd")
+            bjson = bdata.json()
 
-        # Print coin prices to console
-        print("Bitcoin $" + str(bjson['bitcoin']['usd']))
-        print("Ethereum $" + str(bjson['ethereum']['usd']))
-        print("Solana $" + str(bjson['solana']['usd']))
-        print("Sui $" + str(bjson['sui']['usd']))
-        print("Hedera $" + str(bjson['hedera-hashgraph']['usd']))
-        print("BNB $" + str(bjson['binancecoin']['usd']))
-        print("XRP $" + str(bjson['ripple']['usd']))
-        print("Dogecoin $" + str(bjson['dogecoin']['usd']))
-        print("Uniswap $" + str(bjson['uniswap']['usd']))
-        print("Tron $" + str(bjson['tron']['usd']))
-        print("Ton $" + str(bjson['the-open-network']['usd']))
-        print("Cronos $" + str(bjson['crypto-com-chain']['usd']))
-        print("The Graph $" + str(bjson['the-graph']['usd']))
-        print("SafeMoon $" + str(bjson['safemoon-2']['usd']))
-        print("SAFUU $" + str(bjson['safuu']['usd']))
+            # Print coin prices to console
+            print("Bitcoin $" + str(bjson['bitcoin']['usd']))
+            print("Ethereum $" + str(bjson['ethereum']['usd']))
+            print("Solana $" + str(bjson['solana']['usd']))
+            print("Sui $" + str(bjson['sui']['usd']))
+            print("Hedera $" + str(bjson['hedera-hashgraph']['usd']))
+            print("BNB $" + str(bjson['binancecoin']['usd']))
+            print("XRP $" + str(bjson['ripple']['usd']))
+            print("Dogecoin $" + str(bjson['dogecoin']['usd']))
+            print("Uniswap $" + str(bjson['uniswap']['usd']))
+            print("Tron $" + str(bjson['tron']['usd']))
+            print("Ton $" + str(bjson['the-open-network']['usd']))
+            print("Cronos $" + str(bjson['crypto-com-chain']['usd']))
+            print("The Graph $" + str(bjson['the-graph']['usd']))
+            print("SafeMoon $" + str(bjson['safemoon-2']['usd']))
+            print("SAFUU $" + str(bjson['safuu']['usd']))
 
-        # Print message to LED
-        show_message(device, "Bitcoin $" + str(bjson['bitcoin']['usd']) + "          " + \
-                     "Ethereum $" + str(bjson['ethereum']['usd']) + "          " + \
-                     "Solana $" + str(bjson['solana']['usd']) + "          " + \
-                     "Sui $" + str(bjson['sui']['usd']) + "          " + \
-                     "Hedera $" + str(bjson['hedera-hashgraph']['usd']) + "          " + \
-                     "BNB $" + str(bjson['binancecoin']['usd']) + "          " + \
-                     "XRP $" + str(bjson['ripple']['usd']) + "          " + \
-                     "Dogecoin $" + str(bjson['dogecoin']['usd']) + "          " + \
-                     "Uniswap $" + str(bjson['uniswap']['usd']) + "          " + \
-                     "Tron $" + str(bjson['tron']['usd']) + "          " + \
-                     "Ton $" + str(bjson['the-open-network']['usd']) + "          " + \
-                     "Cronos $" + str(bjson['crypto-com-chain']['usd']) + "          " + \
-                     "The Graph $" + str(bjson['the-graph']['usd']) + "          " + \
-                     "SafeMoon $" + str(bjson['safemoon-2']['usd']) + "          " + \
-                     "SAFUU $" + str(bjson['safuu']['usd']) + "          ", \
-                     fill="white", font=proportional(LCD_FONT), scroll_delay=0.04)
+            # Print message to LED
+            show_message(device, "Bitcoin $" + str(bjson['bitcoin']['usd']) + "          " + \
+                        "Ethereum $" + str(bjson['ethereum']['usd']) + "          " + \
+                        "Solana $" + str(bjson['solana']['usd']) + "          " + \
+                        "Sui $" + str(bjson['sui']['usd']) + "          " + \
+                        "Hedera $" + str(bjson['hedera-hashgraph']['usd']) + "          " + \
+                        "BNB $" + str(bjson['binancecoin']['usd']) + "          " + \
+                        "XRP $" + str(bjson['ripple']['usd']) + "          " + \
+                        "Dogecoin $" + str(bjson['dogecoin']['usd']) + "          " + \
+                        "Uniswap $" + str(bjson['uniswap']['usd']) + "          " + \
+                        "Tron $" + str(bjson['tron']['usd']) + "          " + \
+                        "Ton $" + str(bjson['the-open-network']['usd']) + "          " + \
+                        "Cronos $" + str(bjson['crypto-com-chain']['usd']) + "          " + \
+                        "The Graph $" + str(bjson['the-graph']['usd']) + "          " + \
+                        "SafeMoon $" + str(bjson['safemoon-2']['usd']) + "          " + \
+                        "SAFUU $" + str(bjson['safuu']['usd']) + "          ", \
+                        fill="white", font=proportional(LCD_FONT), scroll_delay=0.04)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        print("Waiting 60 seconds before trying again...")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            print("Waiting 60 seconds before trying again...")
+            time.sleep(60)
+    else:
+        print("It's not time yet")
         time.sleep(60)
